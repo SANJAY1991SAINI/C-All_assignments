3ca8f8aca7a61f82ecf4e1a61120b766db3597b9
SANJAY1991SAINI <sanjay1991saini@gmail.com>
2024-03-16 17:00:06 +0530

    1st assignments

---
+++ 1st assignments/q4.c
@@ -0,0 +1,30 @@
+/*  Write a program to print following pattern.
+    a. Using multiple printf statements
+    b. Using single printf statement
+    *
+    **
+    ***
+    ****
+    *****                            */
+
+#include<stdio.h>
+
+int main()
+
+{
+  //Using multiple printf statements
+
+   printf("*\n");
+   printf("**\n");
+   printf("***\n");
+   printf("****\n");
+   printf("*****\n");
+  
+   printf(".........................\n");
+//Using single printf statement
+  
+   printf("*\n**\n***\n****\n*****\n");
+
+
+   return 0;
+}
