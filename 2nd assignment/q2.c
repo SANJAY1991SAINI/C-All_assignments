310f6aa927abafacdf556e16c52de17f17eb7415
SANJAY1991SAINI <sanjay1991saini@gmail.com>
2024-03-18 18:12:23 +0530

    2nd assignment

---
+++ 2nd assignment/q2.c
@@ -0,0 +1,24 @@
+// Input a number and display whether number is Even or Odd.
+
+#include<stdio.h>
+
+int main(void)
+
+{
+  int num;
+  printf("ENTER A NUMBER :");
+  scanf("%d",&num);
+
+  if(num%2==0)
+  printf("NUMBER IS EVEN\n");
+
+  else
+
+
+  printf("NUMBER IS ODD\n");
+
+
+
+
+   return 0;
+}
