7517ca1bdb6030bfbf7f2c9e11aae5d756320d50
SANJAY1991SAINI <sanjay1991saini@gmail.com>
2024-03-18 21:47:13 +0530

    2nd assignment

---
+++ 2nd assignment/q4.c
@@ -0,0 +1,22 @@
+// Write a program to find maximum of two numbers using
+// a. If – else
+// b. conditional operator.
+
+#include<stdio.h>
+
+
+int main(void)
+{
+    int num1,num2;
+	printf("ENTER TWO NUMBER:");
+    scanf("%d %d",&num1,&num2);
+    if(num1>num2)
+
+        printf("first is grater of two number\n");
+
+    else
+        printf("second is grater of two number\n");
+    
+
+    return 0;
+}
